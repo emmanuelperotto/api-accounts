5d6275f2c9365a2e1027299eb1f9a41259c828e4
emmanuelperotto <emmanuel.perotto@pismo.io>
2021-01-10 22:39:51 -0300

    return JWT token when creating account

--- routes/accounts.go
+++ routes/accounts.go
@@ -4,9 +4,17 @@ import (
 	"accounts/entities"
 	"accounts/usecases"
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
 	"log"
 	"net/http"
 )
+
+type response struct {
+	Id          int64  `json:"id"`
+	Agency      string `json:"agency"`
+	Code        string `json:"code"`
+	AccessToken string `json:"access_token"`
+}
 
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
 	var account entities.Account
@@ -24,9 +32,23 @@ func CreateAccount(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"id": account.ID})
+	signedToken, err := token.SignedString([]byte("xablau"))
+
+	if err != nil {
+		log.Println("[JWT Error]", err)
+	}
+
+	respBody := response{
+		Id:          account.ID,
+		Agency:      account.Agency,
+		Code:        account.Code,
+		AccessToken: signedToken,
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(account); err != nil {
-		log.Println("Error when building the response body: ", err)
+	if err := json.NewEncoder(w).Encode(respBody); err != nil {
+		log.Println("[Error when building the response body]", err)
 		return
 	}
 }
